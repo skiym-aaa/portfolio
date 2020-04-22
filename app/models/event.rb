8ce87838718e5fba56edaa6922e0bd984af7cf54
skiym-aaa <y.sakiyama3216@gmail.com>
2020-04-22 11:36:13 +0000

    [Update] 時間表示

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  #belongs_to :idol
+  #belongs_to :place
 
   validates :title, presence: true, length: {maximum: 30}
   validates :body, presence: true, length: {maximum: 50}
