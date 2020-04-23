7e733840c31b6a08634ee1a791e0c94a36d6c3d3
skiym-aaa <y.sakiyama3216@gmail.com>
2020-04-23 12:05:15 +0000

    [Add] アイドル一覧/新規/詳細/編集

--- app/controllers/idols_controller.rb
+++ app/controllers/idols_controller.rb
@@ -1,21 +1,45 @@
 class IdolsController < ApplicationController
 
   def index
+    @idols = Idol.all
   end
 
   def show
+    @idol = Idol.find(params[:id])
   end
 
   def new
+    @idol = Idol.new
   end
 
   def create
+    @idol = Idol.new(idol_params)
+    @idol.user_id = current_user.id
+    if @idol.save
+      redirect_to idol_path(@idol), notice: "アイドルの登録が完了しました！"
+    else
+      render :new
+    end
   end
 
   def edit
+    @idol = Idol.find(params[:id])
   end
 
   def update
+    @idol = Idol.find(params[:id])
+    @idol.user_id = current_user.id
+    if @idol.update(idol_params)
+      redirect_to idol_path(@idol), notice: "アイドルの編集が完了しました！"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def idol_params
+    params.require(:idol).permit(:user_id, :name, :about, :official_site, :image_id)
   end
 
 end
