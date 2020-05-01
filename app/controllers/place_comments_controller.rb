16bd29b485481f1053b80655fa5b1c3269f19174
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-01 07:30:37 +0000

    [Fix] Ajax/場所コメント(レビューも非同期化)

--- app/controllers/place_comments_controller.rb
+++ app/controllers/place_comments_controller.rb
@@ -1,4 +1,6 @@
 class PlaceCommentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @place = Place.find(params[:place_id])
     @place_comment = PlaceComment.new(place_comment_params)
@@ -6,14 +8,18 @@ class PlaceCommentsController < ApplicationController
     @place_comment.place_id = @place.id
     @place_comment.rate = params[:score]
     @place_comment.save
-    flash[:notice] = 'コメントの作成が完了しました！'
+    flash.now[:notice] = 'コメントの作成が完了しました！'
   end
 
   def destroy
     @place_comment = PlaceComment.find(params[:place_id])
     @place = @place_comment.place
     @place_comment.destroy
-    flash[:notice] = 'コメントの削除が完了しました！'
+    flash.now[:notice] = 'コメントの削除が完了しました！'
+    # ajaxのrender用
+    @place = Place.find(params[:id])
+    @place_photos = PlacePhoto.where(place_id: @place.id)
+    @new_place_comment = PlaceComment.new
   end
 
   def place_comment_params
