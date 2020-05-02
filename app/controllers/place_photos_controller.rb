ce65b46a6a1053e6556c9a8f731ffb62d9997a1f
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-02 10:50:43 +0000

    [Fix] 場所写真/バリデーション

--- app/controllers/place_photos_controller.rb
+++ app/controllers/place_photos_controller.rb
@@ -10,7 +10,12 @@ class PlacePhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    @place_photo = PlacePhoto.new(place_photo_params)
+    binding.pry
+    if params[:place_photo].present?
+      @place_photo = PlacePhoto.new(place_photo_params)
+    else
+      @place_photo = PlacePhoto.new(image_id: "")
+    end
     @place_photo.user_id = current_user.id
     @place_photo.place_id = @place.id
     if @place_photo.save
