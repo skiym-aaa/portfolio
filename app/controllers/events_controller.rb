aa5ff913c9cd9d0aa0656ec50d574abcea0c9158
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-05 14:13:35 +0000

    [Fix] イベント/レイアウト

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < InheritedResources::Base
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.all.order(:start_date)
   end
 
   # GET /events/1
