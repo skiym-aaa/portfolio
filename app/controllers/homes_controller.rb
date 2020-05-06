9e528216d2bdbc313f3cf1d31eb5d34ef187fa94
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-06 13:23:53 +0000

    [Add] トップ画面

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,9 @@
 class HomesController < ApplicationController
-  def top; end
+  def top
+    @events = Event.all.order("RANDOM()").limit(3)
+    @idols = Idol.all.order("RANDOM()").limit(3)
+    @places = Place.all.order("RANDOM()").limit(3)
+  end
 
   def about; end
 end
