d9dfb525c6e3049a3b2a52c39ea7984be4e9d00f
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-07 05:02:48 +0000

    rubocop

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,13 +1,13 @@
 class HomesController < ApplicationController
   def top
     if Rails.env.production?
-      @events = Event.all.order("RAND()").limit(3)
-      @idols = Idol.all.order("RAND()").limit(3)
-      @places = Place.all.order("RAND()").limit(3)
+      @events = Event.all.order('RAND()').limit(3)
+      @idols = Idol.all.order('RAND()').limit(3)
+      @places = Place.all.order('RAND()').limit(3)
     elsif Rails.env.development?
-      @events = Event.all.order("RANDOM()").limit(3)
-      @idols = Idol.all.order("RANDOM()").limit(3)
-      @places = Place.all.order("RANDOM()").limit(3)
+      @events = Event.all.order('RANDOM()').limit(3)
+      @idols = Idol.all.order('RANDOM()').limit(3)
+      @places = Place.all.order('RANDOM()').limit(3)
     end
   end
 
