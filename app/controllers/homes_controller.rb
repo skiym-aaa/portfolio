f4e37399d18cdc57f88169eb21fd3c708574a583
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-07 04:31:05 +0000

    debug commit20

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,14 @@
 class HomesController < ApplicationController
   def top
-    @events = Event.all.order("RANDOM()").limit(3)
-    @idols = Idol.all.order("RANDOM()").limit(3)
-    @places = Place.all.order("RANDOM()").limit(3)
+    if Rails.env.production?
+      @events = Event.all.order("RAND()").limit(3)
+      @idols = Idol.all.order("RAND()").limit(3)
+      @places = Place.all.order("RAND()").limit(3)
+    elsif Rails.env.development?
+      @events = Event.all.order("RANDOM()").limit(3)
+      @idols = Idol.all.order("RANDOM()").limit(3)
+      @places = Place.all.order("RANDOM()").limit(3)
+    end
   end
 
   def about; end
