dde6f5c32f852480b58b97d7e6d5742405318e52
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-08 08:59:03 +0000

    [Add] トップ,場所詳細/スライドショー機能

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
   end
   resources :places, only: [:index, :show, :new, :create, :edit, :update] do
-    resources :place_photos, only: [:show, :new, :create]
+    resources :place_photos, only: [:new, :create]
     resource :place_comments, only: [:create, :destroy]
     resource :bookmarks, only: [:create, :destroy]
   end
