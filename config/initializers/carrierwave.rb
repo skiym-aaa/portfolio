3bee61388a347e83907039ab6ef49bc3019acd29
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-09 06:11:26 +0000

    debug commit25

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'ENV['aws_access_key_id']',
-      aws_secret_access_key: 'ENV['aws_secret_access_key']',
+      aws_access_key_id: ENV['aws_access_key_id'],
+      aws_secret_access_key: ENV['aws_secret_access_key'],
       region: 'ap-northeast-1'
     }
 
