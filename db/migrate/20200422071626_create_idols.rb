e708f4684908b3935318a9d77844b1ae508710a6
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-10 10:01:29 +0000

    debug commit34

--- db/migrate/20200422071626_create_idols.rb
+++ db/migrate/20200422071626_create_idols.rb
@@ -3,7 +3,7 @@ class CreateIdols < ActiveRecord::Migration[5.2]
     create_table :idols do |t|
       t.references :user, foreign_key: true
       t.string :name
-      t.text :about, default: "description"
+      t.text :about
       t.string :official_site
       t.string :official_twitter
       t.string :official_youtube
