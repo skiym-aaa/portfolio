8aa7e17d4b34008b77be92f70ebe9be97d599483
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-13 02:58:34 +0000

    [Add] データベース情報3

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,10 +5,10 @@ class Event < ApplicationRecord
   has_many :event_comments, dependent: :destroy
   has_many :event_registers, dependent: :destroy
 
-  enum genre: { リリースイベント: 0, 単独ライブ: 1, 対バン: 2, 生誕祭: 3, 公開収録: 4, その他: 5 }
+  enum genre: { リリースイベント: 0, 単独ライブ: 1, 対バン: 2, 特典会: 3, 生誕祭: 4, 公開収録: 5, その他: 6 }
 
   validates :user_id, presence: true
-  validates :title, presence: true, length: { maximum: 30 }
+  validates :title, presence: true, length: { maximum: 50 }
   validates :body, length: { maximum: 50 }
   validates :start_date, presence: true
   validates :end_date, presence: true
