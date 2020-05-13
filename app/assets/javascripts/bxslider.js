d5f33c9a4c147210578bdbeb4b6fa5fba53318d3
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-13 06:09:54 +0000

    [Modify] レイアウト/トップ画面/おすすめイベントスライド

--- app/assets/javascripts/bxslider.js
+++ app/assets/javascripts/bxslider.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     moveSlides: 1,        // 移動するスライド数
     pause: 3000,          // 自動スライドの待ち時間
     maxSlides: 1,         // 一度に表示させる最大数
-    slideWidth: 360,      // 各スライドの幅
+    slideWidth: 500,      // 各スライドの幅
     randomStart: true,    // 最初に表示するスライドをランダムに設定
     autoHover: true,      // ホバー時に自動スライドを停止
     captions: true,  　　　// キャプション追加
