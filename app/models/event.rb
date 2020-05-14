a625a063fb747a0d9225b1093661c75441817011
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-14 00:31:58 +0000

    [Modify] バリデーション/文字数エラー

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,7 +9,7 @@ class Event < ApplicationRecord
 
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 50 }
-  validates :body, length: { maximum: 50 }
+  # validates :body, length: { maximum: 50 }
   validates :start_date, presence: true
   validates :end_date, presence: true
 
