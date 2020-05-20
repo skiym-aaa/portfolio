3f03b30de51f26c2006c7388af2373e56db296fd
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-20 09:54:36 +0000

    [Add] 各ページのタイトル

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title='')
+    base_title ="idol places"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
