aa17df2d56b66df0dc415eda15ca45fe3448c223
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-21 06:33:38 +0000

    [Modify] 退会済みユーザー表示

--- app/controllers/place_photos_controller.rb
+++ app/controllers/place_photos_controller.rb
@@ -1,5 +1,5 @@
 class PlacePhotosController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[show]
   before_action :set_calender, only: [:new]
 
   def show
