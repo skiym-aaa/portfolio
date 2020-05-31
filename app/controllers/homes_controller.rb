5075dabbf8715150c0f65cc19d39dff3be741ff0
skiym-aaa <y.sakiyama3216@gmail.com>
2020-05-31 08:50:47 +0000

    [Modify] トップ画面/当日イベント情報

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,10 +4,17 @@ class HomesController < ApplicationController
   def top
     # 本番環境で'RANDOM変数は使用できない'
     if Rails.env.production?
+      # 当日のイベント情報を取得
+      @today = Date.today
+      @today_events = Event.all.where(start_date: @today.all_day)
+
       @events = Event.all.order('RAND()').limit(3)
       @idols = Idol.all.order('RAND()').limit(3)
       @places = Place.all.order('RAND()').limit(3)
     else
+      @today = Date.today
+      @today_events = Event.all.where(start_date: @today.all_day)
+
       @events = Event.all.order('RANDOM()').limit(3)
       @idols = Idol.all.order('RANDOM()').limit(3)
       @places = Place.all.order('RANDOM()').limit(3)
