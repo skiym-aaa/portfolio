803bfa1d5e6f992961fcec11602ca302a7f721fe
skiym-aaa <y.sakiyama3216@gmail.com>
2020-06-07 03:06:07 +0000

    debug commit45

--- db/migrate/20200422071650_create_places.rb
+++ db/migrate/20200422071650_create_places.rb
@@ -4,7 +4,7 @@ class CreatePlaces < ActiveRecord::Migration[5.2]
       t.references :user, foreign_key: true
       t.string :name
       t.string :address
-      t.text :about, default: "description"
+      t.text :about
       t.string :official_site
       t.string :image_id
       t.timestamps
